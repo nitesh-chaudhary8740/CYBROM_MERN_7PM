0c73999a9a7860f5401d4a54cc9dde1015353d09
The-Enigma-Code <niteshchaudhary0812@gmail.com>
2025-11-01 19:25:21 +0530

    middlewares

--- server/index.js
+++ server/index.js
@@ -8,6 +8,16 @@ const app = express();
 app.use(cors());
 app.use(express.json())
 app.use(express.urlencoded())
+app.use((req,res,next)=>{ //app level middleware
+    console.log("this is custom(app) level middleware")
+    req.myData="custom request property "
+    next()
+})
+app.use("/hello",(_,_,next)=>{ //path level middleware
+    console.log("path level hello")
+},(req,res)=>{
+    res.send("hello response send")
+}) 
 app.use("/user",router)
 
 connectToDB().then(
